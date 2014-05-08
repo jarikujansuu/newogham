6c05e662441e56eb15da28dee5d931b5548def0e
jarik <kujansuujari@gmail.com>
2014-05-08 14:56:20 +0300

    Added some tests

--- src/test/scala/org/newogham/bbmSpec.scala
+++ src/test/scala/org/newogham/bbmSpec.scala
@@ -9,9 +9,16 @@ import org.scalatest.time.SpanSugar._
 import org.scalatest.concurrent.Timeouts._
 
 class bbmSpec extends FunSpec with ScalaFutures {
+	describe("leagues") {
+		it("should return all leagues") {
+			whenReady(leagues, timeout(10 seconds)) { result ⇒
+				result.isEmpty should be (false)
+			}
+		}
+	}
 	describe("league") {
 		it("should return correct league") {
-			whenReady(league(2449), timeout(1 seconds)) { result ⇒
+			whenReady(league(2449), timeout(10 seconds)) { result ⇒
 				result.id should be(2449)
 				result.name should be("NOBBL")
 				result.meta should be(true)
@@ -23,6 +30,22 @@ class bbmSpec extends FunSpec with ScalaFutures {
 			whenReady(toLeagueTree(League(2449, "NOBBL", true)), timeout(10 seconds)) { result ⇒
 				result.root should be (League(2449, "NOBBL", true))
 				result.children.isEmpty should be (false)
+			}
+		}
+	}
+	describe("matchesForLeague") {
+		it("should get all matches in league") {
+			whenReady(matchesForLeague(2449), timeout(1000 seconds)) { result =>
+				println(result)
+				result.isEmpty should be (false)
+			}
+		}
+	}
+	describe("matchesBetween") {
+		it("should get all matches in league between teams") {
+			whenReady(matchesBetween(2449, "Eagle Warriors", "Kindergarten Nightmares"), timeout(1000 seconds)) { result =>
+				println(result)
+				result.isEmpty should be (false)
 			}
 		}
 	}
